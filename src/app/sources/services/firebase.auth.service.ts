ee5ed192b5b7c96af7b088dd73e83f044cef290d
Mritunjay Kumar <ashupost@gmail.com>
2018-06-03 05:27:27 +0530

    sadasdasdasd

---
+++ src/app/sources/services/firebase.auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
+
+
+@Injectable()
+export class AuthService {
+
+  authState: any = null;
+
+  constructor(private afAuth: AngularFireAuth) {
+            this.afAuth.authState.subscribe((auth) => {
+              this.authState = auth
+            });
+          }
+
+  // Returns true if user is logged in
+  get authenticated(): boolean {
+    return this.authState !== null;
+  }
+
+  // Returns current user data
+  get currentUser(): any {
+    return this.authenticated ? this.authState : null;
+  }
+
+  // Returns
+  get currentUserObservable(): any {
+    return this.afAuth.authState
+  }
+
+  // Returns current user UID
+  get currentUserId(): string {
+    return this.authenticated ? this.authState.uid : '';
+  }
+
+  // Anonymous User
+  get currentUserAnonymous(): boolean {
+    return this.authenticated ? this.authState.isAnonymous : false
+  }
+
+  // Returns current user display name or Guest
+  get currentUserDisplayName(): string {
+    if (!this.authState) { return 'Guest' }
+    else if (this.currentUserAnonymous) { return 'Anonymous' }
+    else { return this.authState['displayName'] || 'User without a Name' }
+  }
+
+
+
+}
