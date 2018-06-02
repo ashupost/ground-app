3e39ad6d40dfa3b707336643f73bb6934e372231
Mritunjay Kumar <ashupost@gmail.com>
2018-06-02 16:03:41 +0530

    Formatting

--- src/app/sources/services/ground-firebasestore.service.ts
+++ src/app/sources/services/ground-firebasestore.service.ts
@@ -1,25 +1,18 @@
 import { Injectable, NgZone } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 import { UserDetails, GeoCordinate, UserStatus } from '../model/userdetails';
-import { AngularFirestore, AngularFirestoreDocument, Action } from 'angularfire2/firestore';
+import { AngularFirestore, Action } from 'angularfire2/firestore';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/combineLatest';
 import 'firebase/firestore';
-import { AngularFireAuth } from 'angularfire2/auth';
-
 import * as firebase from 'firebase/app';
 
 @Injectable()
 export class GroundFirebaseStoreService {
-    constructor(private _angularFirestore: AngularFirestore,
-        private afAuth: AngularFireAuth,
-        private zone: NgZone
+    constructor(private zone: NgZone,
+        private _angularFirestore: AngularFirestore
     ) { }
-
-
-
 
     setSettingData(userId: string, value: string, param: string) {
         let data = {};
@@ -34,11 +27,13 @@ export class GroundFirebaseStoreService {
             case 'age_upper':
                 data = { age_upper: value, timestamp: timestamp };
                 break;
-    
+
             default:
                 data = { param: value, timestamp: timestamp };
         }
-        this._angularFirestore.collection<UserDetails>('settings').doc(userId).set(data, { merge: true });
+        this.zone.run(() => {
+            this._angularFirestore.collection<UserDetails>('settings').doc(userId).set(data, { merge: true });
+        });
     }
 
 
@@ -59,22 +54,25 @@ export class GroundFirebaseStoreService {
             default:
                 data = { param: value, timestamp: timestamp };
         }
-        this._angularFirestore.collection<UserDetails>('users').doc(userId).set(data, { merge: true });
+        this.zone.run(() => {
+            this._angularFirestore.collection<UserDetails>('users').doc(userId).set(data, { merge: true });
+        });
     }
 
     updatePhotoURL(userId: string, value: string) {
         let timestamp = firebase.firestore.FieldValue.serverTimestamp();
-        this._angularFirestore.collection<UserDetails>('users')
-            .doc(userId)
-            .update({
-                photoURL: value,
-                timestamp: timestamp
-            });
+        this.zone.run(() => {
+            this._angularFirestore.collection<UserDetails>('users')
+                .doc(userId)
+                .update({
+                    photoURL: value,
+                    timestamp: timestamp
+                });
+        });
     }
 
-  
-    getLatestGeoCordinidateByUsers(userid: string): Observable<GeoCordinate[]> {
 
+    getLatestGeoCordinidateByUsers(userid: string): Observable<GeoCordinate[]> {
         return this._angularFirestore.collection<UserDetails>('users')
             .doc(userid).collection('geolocation', ref => {
                 let query: firebase.firestore.Query = ref;
@@ -93,7 +91,7 @@ export class GroundFirebaseStoreService {
 
     getMessages(myId: string, otherId: string): Observable<any[]> {
         let timestamp$ = new BehaviorSubject(null);
-        let colorFilter$ = new BehaviorSubject(null);
+        // let colorFilter$ = new BehaviorSubject(null);
         return Observable.combineLatest(
             timestamp$
         ).switchMap(([timestamp]) =>
@@ -119,7 +117,7 @@ export class GroundFirebaseStoreService {
 
     sendMessage(toId: string, fromId: string, newMessage: string) {
         this.zone.run(() => {
-            const merge: firebase.firestore.SetOptions = { merge: false };
+            // const merge: firebase.firestore.SetOptions = { merge: false };
             let timestamp = firebase.firestore.FieldValue.serverTimestamp();
             this._angularFirestore.collection<UserDetails>('users')
                 .doc(toId).collection('messages').doc(fromId).collection('chat')
@@ -147,16 +145,18 @@ export class GroundFirebaseStoreService {
     }
 
     addUsers(userDetails: UserDetails) {
-        this._angularFirestore.collection<UserDetails>('users')
-            .doc(userDetails.uid).snapshotChanges()
-            .subscribe((value: Action<firebase.firestore.DocumentSnapshot>) => {
-                if (value.payload.exists !== true) {
-                    let data = JSON.parse(JSON.stringify(userDetails));
-                    data.timestamp = firebase.firestore.FieldValue.serverTimestamp();
-                    this._angularFirestore.collection<UserDetails>('users')
-                        .doc(userDetails.uid).set(data);
-                }
-            });
+        this.zone.run(() => {
+            this._angularFirestore.collection<UserDetails>('users')
+                .doc(userDetails.uid).snapshotChanges()
+                .subscribe((value: Action<firebase.firestore.DocumentSnapshot>) => {
+                    if (value.payload.exists !== true) {
+                        let data = JSON.parse(JSON.stringify(userDetails));
+                        data.timestamp = firebase.firestore.FieldValue.serverTimestamp();
+                        this._angularFirestore.collection<UserDetails>('users')
+                            .doc(userDetails.uid).set(data);
+                    }
+                });
+        });
     }
 
     public getUsers(): Observable<UserDetails[]> {
