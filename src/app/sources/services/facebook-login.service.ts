02de0426a360c3d50dd558596532f1209b0b3f8b
Mritunjay Kumar <ashupost@gmail.com>
2018-06-08 23:09:50 +0530

    Tab2 Done

--- src/app/sources/services/facebook-login.service.ts
+++ src/app/sources/services/facebook-login.service.ts
@@ -2,32 +2,30 @@ import { Injectable } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
-
 import * as firebase from 'firebase/app';
 
 @Injectable()
 export class FaceBookLoginService {
     constructor(
-        private _platform: Platform,
-        private facebook: Facebook,
-        private afAuth: AngularFireAuth) {
+        private __platform: Platform,
+        private __facebook: Facebook,
+        private __afAuth: AngularFireAuth) {
     }
 
     facebookLogin() {
-        if (this._platform.is('cordova')) {
+        if (this.__platform.is('cordova')) {
             this.facebookLoginCordova();
         } else {
             this.facebookLoginWeb();
         }
     }
 
-
     async facebookLoginCordova(): Promise<void> {
         try {
-            this.facebook.login(['public_profile', 'email'])
+            this.__facebook.login(['public_profile', 'email'])
                 .then((response: FacebookLoginResponse) => {
                     let creadet = firebase.auth.FacebookAuthProvider.credential(response.authResponse.accessToken);
-                    return this.afAuth.auth.signInWithCredential(creadet);
+                    return this.__afAuth.auth.signInWithCredential(creadet);
                 });
         } catch (err) {
             alert('FaceBook Login error Cordova' + err);
@@ -37,13 +35,13 @@ export class FaceBookLoginService {
     async facebookLoginWeb(): Promise<void> {
         try {
             const provider = new firebase.auth.FacebookAuthProvider();
-            return await this.afAuth.auth.signInWithPopup(provider);
+            return await this.__afAuth.auth.signInWithPopup(provider);
         } catch (err) {
             alert('FaceBook Login error web' + err);
         }
     }
 
     logout() {
-        this.afAuth.auth.signOut();
+        this.__afAuth.auth.signOut();
     }
 }
