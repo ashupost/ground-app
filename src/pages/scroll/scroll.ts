1a8ca4974d598c9fe8dd1f0d0a265af38ae42c41
Mritunjay Kumar <ashupost@gmail.com>
2018-06-10 06:59:25 +0530

    Scroll working

--- src/pages/scroll/scroll.ts
+++ src/pages/scroll/scroll.ts
@@ -17,56 +17,55 @@ import * as _ from 'lodash'
 @Component({
   selector: 'page-scroll',
   templateUrl: 'scroll.html',
-  
+
 })
 export class ScrollPage implements OnInit {
   items$: Observable<UserDetails[]>;
   movies = new BehaviorSubject([]);
-  
+
   batch = 2        // size of each query
   lastKey = ''      // key to offset next query from
   finished = false  // boolean when end of database is reached
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private movieService: MovieService) { }
-  
+  constructor(public navCtrl: NavController, public navParams: NavParams, private movieService: MovieService) { }
+
   ngOnInit() {
     this.getMovies();
   }
-  
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad ScrollPage');
-    this.items$= this.movieService.getUsers1();
-   }
+    this.items$ = this.movieService.getUsers1();
+  }
 
-   doInfinite(infiniteScroll) {
+  doInfinite(): Promise<any> {
     console.log('Begin async operation');
-
-    setTimeout(() => {
-      this.getMovies();
-
-      console.log('Async operation has ended');
-      infiniteScroll.complete();
-    }, 5000);
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log('Async operation has ended');
+        resolve(this.getMovies());
+      }, 500);
+    })
   }
 
   private getMovies(key?) {
     if (this.finished) return
-    this.movieService.getUsers(this.batch+1, this.lastKey)
-        .do(movies => {
-          /// set the lastKey in preparation for next query
-          this.lastKey = _.last(movies).uid;
-          const newMovies = _.slice(movies, 0, this.batch)
-          /// Get current movies in BehaviorSubject
-          const currentMovies = this.movies.getValue();
-          /// If data is identical, stop making queries
-          if (this.lastKey == _.last(newMovies).uid) {
-            this.finished = true
-          }
-          /// Concatenate new movies to current movies
-          this.movies.next( _.concat(currentMovies, newMovies) );
-        })
-        .take(1)
-        .subscribe();
+    this.movieService.getUsers(this.batch + 1, this.lastKey)
+      .do(movies => {
+        /// set the lastKey in preparation for next query
+        this.lastKey = _.last(movies).uid;
+        const newMovies = _.slice(movies, 0, this.batch)
+        /// Get current movies in BehaviorSubject
+        const currentMovies = this.movies.getValue();
+        /// If data is identical, stop making queries
+        if (this.lastKey == _.last(newMovies).uid) {
+          this.finished = true
+        }
+        /// Concatenate new movies to current movies
+        this.movies.next(_.concat(currentMovies, newMovies));
+      })
+      .take(1)
+      .subscribe();
   }
 }
 
