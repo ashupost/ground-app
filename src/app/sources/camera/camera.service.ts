12f2d4374eebcff56edff7373bf6091ccb714f1e
Mritunjay Kumar <ashupost@gmail.com>
2018-06-13 03:11:55 +0530

    Done

--- src/app/sources/camera/camera.service.ts
+++ src/app/sources/camera/camera.service.ts
@@ -7,7 +7,7 @@ export class CameraService {
     base64Image: any;
 
     constructor(private __camera: Camera) { }
-    takePhoto() {
+    async takePhoto() {
         const options: CameraOptions = {
             quality: 50,
             destinationType: this.__camera.DestinationType.DATA_URL,
@@ -15,11 +15,11 @@ export class CameraService {
             mediaType: this.__camera.MediaType.PICTURE
         };
 
-        return this.__camera.getPicture(options);
+        return await this.__camera.getPicture(options);
     }
 
-    selectImageFromCamera(): Promise<any> {
-        return new Promise((resolve, reject) => {
+    async  selectImageFromCamera(): Promise<any> {
+        return await new Promise((resolve, reject) => {
             let cameraOptions: CameraOptions = {
                 destinationType: this.__camera.DestinationType.DATA_URL,
                 quality: 100,
@@ -27,13 +27,13 @@ export class CameraService {
                 mediaType: this.__camera.MediaType.PICTURE
             };
             this.__camera.getPicture(cameraOptions).then((data) => {
-                    resolve("data:image/jpeg;base64," + data);
-                }, (error) => { reject(error); });
+                resolve("data:image/jpeg;base64," + data);
+            }, (error) => { reject(error); });
         });
     }
 
-    selectImageFromGallary(): Promise<any> {
-        return new Promise((resolve, reject) => {
+    async selectImageFromGallary(): Promise<any> {
+        return await new Promise((resolve, reject) => {
             let cameraOptions: CameraOptions = {
                 sourceType: this.__camera.PictureSourceType.PHOTOLIBRARY,
                 destinationType: this.__camera.DestinationType.DATA_URL,
